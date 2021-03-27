4db802c3ae2be648fa53d5823d8e04024c25df74
Aidan Miles <aidan.miles@gmail.com>
2021-03-27 11:42:58 +1100

    style: remove unnecessary backslashes

--- bash/_bootstrap.fish
+++ bash/_bootstrap.fish
@@ -1,5 +1,5 @@
 begin
     # link bashrc & bash_profile
-    link_file $SRC/bashrc $HOME/.bashrc $BACKUP/(basename $SRC/)
-    link_file $SRC/bash_profile $HOME/.bash_profile $BACKUP/(basename $SRC/)
+    link_file $SRC/bashrc $HOME/.bashrc $BACKUP/(basename $SRC)
+    link_file $SRC/bash_profile $HOME/.bash_profile $BACKUP/(basename $SRC)
 end
--- zsh/_bootstrap.fish
+++ zsh/_bootstrap.fish
@@ -1,6 +1,6 @@
 begin
     # link zshrc
-    link_file $SRC/zshrc $HOME/.zshrc $BACKUP/(basename $SRC/)
+    link_file $SRC/zshrc $HOME/.zshrc $BACKUP/(basename $SRC)
 
     # create ~/.zsh directory
     mkdir -p $HOME/.zsh
