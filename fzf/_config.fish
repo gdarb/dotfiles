c764335e4de16ea3807261c750309b7221dace9d
Aidan Miles <aidan.miles@gmail.com>
2021-11-12 20:49:38 +1100

    feat(fzf): use nord theme

--- fzf/_config.fish
+++ fzf/_config.fish
@@ -5,4 +5,7 @@ if type -q fzf
     if type -q fd
         set -gx FZF_DEFAULT_COMMAND "fd --type f"
     end
+
+    # https://github.com/junegunn/fzf/blob/master/ADVANCED.md#color-themes
+    set -gx FZF_DEFAULT_OPTS "--color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1"
 end
