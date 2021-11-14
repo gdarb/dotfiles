f1d22ac3e7c0481385f616d8516c5ca2d368faa9
Aidan Miles <aidan.miles@gmail.com>
2021-11-14 11:55:30 +1100

    feat(fzf): add color to default command

--- fzf/_config.fish
+++ fzf/_config.fish
@@ -3,9 +3,9 @@
 if type -q fzf
     # use `fd` instead of `find`
     if type -q fd
-        set -gx FZF_DEFAULT_COMMAND "fd --type f"
+        set -gx FZF_DEFAULT_COMMAND "fd --type f --color=always"
     end
 
     # https://github.com/junegunn/fzf/blob/master/ADVANCED.md#color-themes
-    set -gx FZF_DEFAULT_OPTS "--color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1"
+    set -gx FZF_DEFAULT_OPTS "--ansi --color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1"
 end
