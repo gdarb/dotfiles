ff615b41fb9f19136bc1a3ddccc016694e9b014f
Aidan Miles <aidan.miles@gmail.com>
2021-11-24 23:01:51 +1100

    feat(jump): set `$JUMP_HOME`

--- jump/_config.fish
+++ jump/_config.fish
@@ -1,6 +1,9 @@
 # https://github.com/gsamokovarov/jump
 
-# initialise jump
 if type -q jump
+    # set $JUMP_HOME
+    not set -q XDG_CONFIG_HOME && set -gx JUMP_HOME $HOME/.config/jump
+
+    # initialise jump
     status --is-interactive; and source (jump shell fish | psub)
 end
