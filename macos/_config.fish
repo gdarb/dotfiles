cd38e9875415fb8aad5f2f21a540bb156b9387f9
Aidan Miles <aidan.miles@gmail.com>
2022-01-17 10:22:41 +1100

    feat(macos): add `tailscale` alias (#71)

--- macos/_config.fish
+++ macos/_config.fish
@@ -6,4 +6,13 @@ switch (uname)
             defaults write com.apple.dock ResetLaunchPad -bool true
             killall Dock
         end
+
+        # add tailscale alias
+        set -l tailscale_executable /Applications/Tailscale.app/Contents/MacOS/Tailscale
+
+        if test -x $tailscale_executable
+            function tailscale --inherit-variable tailscale_executable
+                $tailscale_executable $argv
+            end
+        end
 end
