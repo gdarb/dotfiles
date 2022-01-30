97e741d47fded52ad58e120422925e93444f0575
Aidan Miles <aidan.miles@gmail.com>
2022-01-30 22:11:52 +1100

    feat(vivid): add compatibility with v0.8.0
    * https://github.com/sharkdp/vivid/releases/tag/v0.8.0

--- vivid/_bootstrap.fish
+++ vivid/_bootstrap.fish
@@ -1,6 +1,6 @@
 begin
     check_install vivid
 
-    # link nord.yml
-    link_file $SRC/nord.yml $XDG_CONFIG_HOME/vivid/themes/nord.yml $BACKUP/(basename $SRC)
+    # link nord2.yml
+    link_file $SRC/nord2.yml $XDG_CONFIG_HOME/vivid/themes/nord2.yml $BACKUP/(basename $SRC)
 end
