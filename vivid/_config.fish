a82448b9d8a05650090e1d678294ad8e7ad36f8c
Aidan Miles <aidan.miles@gmail.com>
2022-01-31 08:49:03 +1100

    feat(vivid): add compatibility with v0.8.0 (#73)
    * https://github.com/sharkdp/vivid/releases/tag/v0.8.0

--- vivid/_config.fish
+++ vivid/_config.fish
@@ -2,5 +2,14 @@
 
 # use `vivid` to set `LS_COLORS`
 if type -q vivid
-    set -gx LS_COLORS (vivid generate nord)
+    set -gx LS_COLORS (vivid generate nord2)
+
+    function vivid-themes -d "preview vivid themes in the current directory"
+        for theme in (vivid themes)
+            echo (set_color --bold)"Theme: $theme"
+            set -lx LS_COLORS (vivid generate $theme)
+            ls -a
+            echo
+        end
+    end
 end
