e088325fe3d222565804f8cb6a59fdde172723a5
Aidan Miles <aidan.miles@gmail.com>
2022-02-17 11:11:22 +1100

    feat(ssh): use 1Password for SSH (#75)

--- ssh/_config.fish
+++ ssh/_config.fish
@@ -1,10 +1,16 @@
 # https://www.openssh.com
 
-set -l ssh_key $HOME/.ssh/id_rsa
+# https://developer.1password.com/docs/ssh
+set -l 1password_socket
 
-# add ssh key to ssh-agent using the built-in macOS ssh tool
-# to allow storing of the keyphrase in the macOS Keychain
-if test (uname) = Darwin
-    and test -e $ssh_key
-    /usr/bin/ssh-add -q --apple-use-keychain $ssh_key
+switch (uname)
+    case Darwin
+        set 1password_socket "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
+    case Linux
+        set 1password_socket "$HOME/.1password/agent.sock"
+end
+
+if test -S $1password_socket
+    set -xg SSH_AUTH_SOCK $1password_socket
 end
