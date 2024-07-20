61ca28565e752bcadf7b0d57473fe80aae80bdf2
Aidan Miles <aidan.miles@gmail.com>
2024-07-20 21:26:28 +1000

    fix(fzf): use updated shell completion command (#89)
    This was added in fzf 0.48.0

--- fzf/_bootstrap.fish
+++
@@ -1,9 +0,0 @@
-begin
-    check_install fzf
-
-    switch (uname)
-        case Darwin
-            eval (brew --prefix)/opt/fzf/install --xdg --no-bash --no-zsh --key-bindings --completion --update-rc
-            or abort "could not run" (set_color --italics magenta)(brew --prefix)/opt/fzf/install(set_color normal)
-    end
-end
